d903d2d6b42ecc8ef37188596f1235d8e76f96b9
khenryjoe <joseph.ketterer@gmail.com>
2020-01-03 18:33:41 +0000

    prettify

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
-const {Engine, Mouse, Render, Runner, World, Bodies, MouseConstraint} = Matter;
+const {
+  Engine,
+  Mouse,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  MouseConstraint
+} = Matter;
 
 const width = 800;
 const height = 600;
 
 const engine = Engine.create();
-const {world} = engine;
+const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    option: {
-        width: width,
-        height: height,
-        wireframes: false,
-        showAngleIndicator: false
-        
-    }
-})
+  element: document.body,
+  engine: engine,
+  option: {
+    width: width,
+    height: height,
+    wireframes: false,
+    showAngleIndicator: false
+  }
+});
 
 Render.run(render);
-Runner.run(Runner.create(), engine)
-World.add(world, MouseConstraint.create(engine,{
+Runner.run(Runner.create(), engine);
+World.add(
+  world,
+  MouseConstraint.create(engine, {
     mouse: Mouse.create(render.canvas)
-}))
-
+  })
+);
 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
-  ];
-  World.add(world, walls);
-  for(let i =0; i < 20; i++){
-  if(Math.random() < 0.5){
-  
-  World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
- 
-}
-else{
-    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 50, 50));
-}
-
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
+for (let i = 0; i < 20; i++) {
+  if (Math.random() < 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  }
 }
